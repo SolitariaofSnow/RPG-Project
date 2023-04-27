69e1a18c6c94e5d1c467753ba807dd107ea37576
SolitariaOfSnow <monikaolmstead@gmail.com>
2023-04-27 09:06:27 -0700

    modified PlayerController

--- RPG Project/Assets/PlayerController.cs
+++ RPG Project/Assets/PlayerController.cs
@@ -5,18 +5,15 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-void OnMove(InputValue movementValue) {
-        movementInput = movementValue.Get<Vector2>();
-     }
 
 
 
     public float moveSpeed = 1f;
-    public float collisionOffset = 0.05f; 
+    public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
 
-    Vector2 movementInput; 
-    Rigidbody2D rb; 
+    Vector2 movementInput;
+    Rigidbody2D rb;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
@@ -24,41 +21,50 @@ void OnMove(InputValue movementValue) {
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
-
-    private void FixedUpdate() {
-        if(movementInput != Vector2.zero){
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
             bool success = TryMove(movementInput);
 
-            if(!success) {
-                success = TryMove (new Vector2(movementInput.x,0));
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
 
-                if(!success) {
+                if (!success)
+                {
                     success = TryMove(new Vector2(0, movementInput.y));
                 }
             }
 
         }
 
-    } 
-    
-    private bool TryMove(Vector2 direction) {
-    
-          int count = rb.Cast(
-                direction,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+    }
 
-                if(count == 0) {
+    private bool TryMove(Vector2 direction)
+    {
 
-                 rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-                 return true;
-                }else
-                 
-                    return false;
-                 }
-                }
+        int count = rb.Cast(
+              direction,
+              movementFilter,
+              castCollisions,
+              moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+        if (count == 0)
+        {
+
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+
+            return false;
+    }
 
 
-     
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
