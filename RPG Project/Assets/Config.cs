dd8c6381be079b800886a25aa9b091c9a31b85a3
reelbeelveel <k.belveal17@gmail.com>
2023-04-28 13:14:28 -0400

    SaveFile made static

--- RPG Project/Assets/Config.cs
+++ RPG Project/Assets/Config.cs
@@ -2,5 +2,5 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Config {
-    public string SaveFile { get; } = "save.json";
+    public static string SaveFile { get; } = "save.json";
 }
