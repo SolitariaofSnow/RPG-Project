667e145b175464624fd0be268166d3a5e2e6165a
SolitariaOfSnow <monikaolmstead@gmail.com>
2023-04-28 09:23:36 -0700

    Fixed Serialization Capitalization and added BattleScripts.

---
+++ RPG Project/Assets/BattleScripts/BaseEnemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+  [System.Serializable]
+public class BaseEnemy 
+{
+  public string name;
+
+  public enum Type
+  {
+    grass,
+    fire,
+    water,
+    electric
+  }
+  public enum Rarity
+  {
+    common,
+    uncommon,
+    rare,
+    mythic,
+  }
+  public Type EnemyType;
+  public Rarity rarity;
+  public float baseHP;
+  public float curHP;
+
+  public float baseMP;
+  public float curMP;
+
+  public float baseATK;
+  public float curATK;
+
+  public float baseDEF;
+  public float curDEF;
+}
---
+++ RPG Project/Assets/BattleScripts/BaseHero.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseHero 
+{
+   public string name; 
+    
+   public float baseHP;
+   public float curHP;
+
+   public float baseMP;
+   public float curMP;
+
+   public int stamina; //endurance
+   public int intellect;
+   public int dexterity;
+   public int agility; //evasion
+    
+}
