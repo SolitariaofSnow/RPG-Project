89e68dfc7127ddf31cdae812fbcc8a84be1e9da8
reelbeelveel <k.belveal17@gmail.com>
2023-04-28 13:32:24 -0400

    changed save file to const

--- RPG Project/Assets/Config.cs
+++ RPG Project/Assets/Config.cs
@@ -2,5 +2,5 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Config {
-    public static string SaveFile { get; } = "save.json";
+    public const string SaveFile = "save.json";
 }
