d49f239b5dbc726e3e133b51ef863f7809d054ca
SolitariaOfSnow <monikaolmstead@gmail.com>
2023-04-28 18:17:49 -0700

    We have a moving turn bar!

--- RPG Project/Assets/BattleScripts/EnemyStateMachine.cs
+++ RPG Project/Assets/BattleScripts/EnemyStateMachine.cs
@@ -5,15 +5,69 @@ using UnityEngine;
 public class EnemyStateMachine : MonoBehaviour {
 
     public BaseEnemy enemy;
-    // Start is called before the first frame update
+
+     public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    };
+
+    public TurnState CurrentState; 
+    //for the progress bar 
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+
     void Start()
     {
-        
+        CurrentState = TurnState.PROCESSING; 
     }
 
     // Update is called once per frame
     void Update()
     {
+         switch(CurrentState)
+        {
+            case(TurnState.PROCESSING):
+                    UpgradeProgressBar ();
+            break;
+
+            case(TurnState.ADDTOLIST):
+
+            break;
+
+            case(TurnState.WAITING):
+
+            break;
+
+            case(TurnState.SELECTING):
+
+            break;
+
+            case(TurnState.ACTION):
+
+            break;
+
+            case(TurnState.DEAD):
+
+            break;
+
+            default:
+                throw new System.ArgumentException("Bad Hero State");
+            break;
         
+        };
     }
+     void UpgradeProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+        if(cur_cooldown >= max_cooldown)
+        {
+            CurrentState = TurnState.ADDTOLIST; 
+        }
+    }
+
 }
--- RPG Project/Assets/BattleScripts/HeroStateMachine.cs
+++ RPG Project/Assets/BattleScripts/HeroStateMachine.cs
@@ -1,21 +1,76 @@
+using System.Timers;
+using System;
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroStateMachine : MonoBehaviour {
 
    public BaseHero hero; 
 
+   public enum TurnState
+    {
+        PROCESSING,
+        ADDTOLIST,
+        WAITING,
+        SELECTING,
+        ACTION,
+        DEAD
+    };
 
-    // Start is called before the first frame update
+    public TurnState CurrentState; 
+    //for the progress bar 
+    private float cur_cooldown = 0f;
+    private float max_cooldown = 5f;
+    public Image ProgressBar;
     void Start()
     {
-        
+        CurrentState = TurnState.PROCESSING; 
     }
-
-    // Update is called once per frame
     void Update()
     {
+        switch(CurrentState)
+        {
+            case(TurnState.PROCESSING):
+                    UpgradeProgressBar ();
+            break;
+
+            case(TurnState.ADDTOLIST):
+
+            break;
+
+            case(TurnState.WAITING):
+
+            break;
+
+            case(TurnState.SELECTING):
+
+            break;
+
+            case(TurnState.ACTION):
+
+            break;
+
+            case(TurnState.DEAD):
+
+            break;
+
+            default:
+                throw new System.ArgumentException("Bad Hero State");
+            break;
         
+        };
+    }
+    void UpgradeProgressBar()
+    {
+        cur_cooldown = cur_cooldown + Time.deltaTime;
+        float calc_cooldown = cur_cooldown / max_cooldown; 
+        ProgressBar.transform.localScale = new Vector3(Mathf.Clamp(calc_cooldown, 0, 1), ProgressBar.transform.localScale.y, ProgressBar.transform.localScale.z);
+        if(cur_cooldown >= max_cooldown)
+        {
+            CurrentState = TurnState.ADDTOLIST; 
+        }
     }
 }
